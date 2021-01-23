adbbaae8c0859969e866979aaf931b07de3b8bae
kevindevani <kevindevani9784@gmail.com>
2021-01-23 16:39:50 +0530

    Add SimpleAnimation package complete

--- src/components/HomeComponent/HomeComponent.js
+++ src/components/HomeComponent/HomeComponent.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect, useRef} from 'react';
 import {View, Text, TextInput, TouchableOpacity, Animated} from 'react-native';
 
+import {SimpleAnimation} from 'react-native-simple-animations';
+
 import {styles} from './styles';
 import {clearStorage, readData, storeData} from '../../utils/storage';
 import {AGE_KEY, HEIGHT_KEY} from '../../utils/constant';
@@ -65,34 +67,59 @@ const HomeComponent = () => {
 
   return (
     <Animated.View style={[styles.Container, {opacity: fadeAnim}]}>
-      <View style={styles.TextView}>
-        <TextInput
-          style={styles.input}
-          placeholder="Age is just Number!"
-          keyboardType={'numeric'}
-          value={age}
-          onChangeText={onChangeText}
-          onSubmitEditing={onSubmitEditing}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Height matters!"
-          keyboardType={'numeric'}
-          value={height}
-          onChangeText={handleHeight}
-        />
-        <TouchableOpacity onPress={handleStoreAge}>
-          <Animated.View style={[styles.buttonView]}>
-            <Text style={styles.buttonText}>Store</Text>
-          </Animated.View>
-        </TouchableOpacity>
-      </View>
-      <Text style={styles.ageText}>Your Age is : {storeAge} </Text>
-      <Text style={styles.ageText}>Your Height is : {storageH} </Text>
-      <TouchableOpacity onPress={handleClear}>
-        <View style={styles.buttonView}>
-          <Text style={styles.buttonText}>Clear Age</Text>
+      <SimpleAnimation
+        delay={50}
+        movementType="slide"
+        distance={100}
+        direction="down"
+        duration={1000}>
+        <View style={styles.TextView}>
+          <TextInput
+            style={styles.input}
+            placeholder="Age is just Number!"
+            keyboardType={'numeric'}
+            value={age}
+            onChangeText={onChangeText}
+            onSubmitEditing={onSubmitEditing}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Height matters!"
+            keyboardType={'numeric'}
+            value={height}
+            onChangeText={handleHeight}
+          />
         </View>
+      </SimpleAnimation>
+      <TouchableOpacity onPress={handleStoreAge}>
+        <SimpleAnimation
+          delay={50}
+          movementType="spring"
+          distance={100}
+          direction="right"
+          duration={1000}>
+          <View style={[styles.buttonView]}>
+            <Text style={styles.buttonText}>Store</Text>
+          </View>
+        </SimpleAnimation>
+      </TouchableOpacity>
+      <SimpleAnimation delay={50} duration={1000} fade staticType="zoom">
+        <Text style={styles.ageText}>Your Age is : {storeAge} </Text>
+      </SimpleAnimation>
+      <SimpleAnimation delay={50} duration={1000} fade staticType="bounce">
+        <Text style={styles.ageText}>Your Height is : {storageH} </Text>
+      </SimpleAnimation>
+      <TouchableOpacity onPress={handleClear}>
+        <SimpleAnimation
+          delay={50}
+          movementType="spring"
+          distance={100}
+          direction="left"
+          duration={1000}>
+          <View style={styles.buttonView}>
+            <Text style={styles.buttonText}>Clear Age</Text>
+          </View>
+        </SimpleAnimation>
       </TouchableOpacity>
     </Animated.View>
   );
